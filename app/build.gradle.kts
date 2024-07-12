d649065a1baa218eb43240a860d4e7879e6f2d03
marcelocrs1 <marcelocrs@gmail.com>
2024-07-12 17:04:23 +0100

    Create new app exercise

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,8 @@ import java.util.Properties
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    kotlin("plugin.serialization") version "2.0.0"
+
 }
 
 android {
